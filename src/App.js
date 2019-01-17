b559b037dcf2f5a9e50aba356d19942bf31f87ca
Johannes Schmitt <Johannes@power-polo.de>
2019-01-17 16:16:28 +0100

    Failstate

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import welcomeClass from "./welcomeClass";
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>GELBER</h1>
         </header>
+        <welcomeClass name={"Herr Meier!"}></welcomeClass>
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
